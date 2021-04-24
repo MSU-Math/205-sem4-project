1809fc87d5832f8bf710a1905ea04b6fe4a92ecd
Maxim Krivchikov <maxim.krivchikov@gmail.com>
2021-04-24 11:15:05 +0300

    2021-04-19 messages

--- server/main.c
+++ server/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -14,15 +15,51 @@ static struct {
     int is_terminating;
 } STATE = {0};
 
+#define METHOD_LENGTH 8
+
 struct request {
     int authorization_size;
     char *authorization;
-    char *method;
+    char method[METHOD_LENGTH];
     char *path;
+    int content_length;
 };
 
+struct message {
+    char *username;
+    char *text;
+    struct timeval time;
+};
+
+struct message_list {
+    struct message *messages;
+    int size;
+    int capacity;
+};
+
+#define BLOCK 8
+// Добавить сообщение к списку
+int message_list_add(struct message_list *list, char *username, char *text)
+{
+    if (list->capacity <= list->size) {
+        struct message *expanded = realloc(list->messages, (list->capacity + BLOCK) * sizeof(struct message));
+        if (expanded) {
+            list->messages = expanded;
+        } else {
+            return 1;
+        }
+    }
+
+    struct message *new_message = &list->messages[list->size];
+    new_message->username = username;
+    new_message->text = text;
+    list->size += 1;
+    gettimeofday(&new_message->time, NULL);
+}
+
 int return_401(FILE *output);
-int return_200(FILE *output, const struct request *request);
+int return_200(FILE *output, const struct request *request, const struct message_list *messages);
+int add_message(FILE *input, const struct request *request, struct message_list *messages);
 int parse_request(FILE *input, struct request *request);
 #define REQUEST_SIZE 4096
 
@@ -44,6 +81,9 @@ int main(void)
 {
     int error;
     struct addrinfo *result;
+    struct message_list messages = { 0 };
+
+    message_list_add(&messages, "admin", "server started");
 
     struct addrinfo hints = {
         .ai_family = AF_INET,
@@ -134,7 +174,10 @@ int main(void)
         if (!request.authorization) {
             return_401(output);
         } else {
-            return_200(output, &request);
+            if (!strcmp("POST", &request.method)) {
+                add_message(input, &request, &messages);
+            }
+            return_200(output, &request, &messages);
             free(request.authorization);
         }
         fclose(output);
@@ -162,6 +205,11 @@ int parse_request(FILE *input, struct request *request)
 {
     request->authorization = NULL;
 
+    // считываем метод HTTP-запроса
+    fscanf(input, "%8[^ ]", &request->method);
+    // пропускаем символы до конца строки
+    while (fgetc(input) > '\n') { }
+
     char *line = NULL;
     size_t size = 0;
     while (getline(&line, &size, input) > 0 && line[0] > ' ') {
@@ -169,19 +217,42 @@ int parse_request(FILE *input, struct request *request)
             request->authorization = line;
             line = NULL;
             size = 0;
+        } else {
+            if (sscanf(line, "Content-Length: %d", &request->content_length) == 1) {
+                // printf("Read Content-Length: %d\n", request->content_length);
+            }
         }
+        // printf("Header: %s\n", line);
     }
     return 0;
 }
 
-int return_200(FILE *output, const struct request *request)
+int return_200(FILE *output, const struct request *request, const struct message_list *messages)
 {
     const char *response = "HTTP/1.1 200 OK\r\n"
         "Content-Type: text/plain, encoding=utf-8\r\n"
         "\r\n"
         "Hello, ";
-    
+     
     int status = fputs(response, output) > 0;
     status |= fputs(request->authorization, output) > 0;
+    status |= fputs("\n", output) > 0;
+
+    for (int i = 0; i < messages->size; i++)
+    {
+        const struct message *message = &messages->messages[i];
+        fprintf(output, "%ld: %s: %s\n", message->time.tv_sec, message->username, message->text);
+    }
     return status;
+}
+
+int add_message(FILE *input, const struct request *request, struct message_list *messages)
+{
+    char *text = malloc(request->content_length + 1);
+    memset(text, 0, request->content_length + 1);
+    fread(text, request->content_length, 1, input);
+    
+    // домашнее задание про base64-декодирование подключать здесь:
+    char *username = strdup(request->authorization);
+    return message_list_add(messages, username, text);
 }
